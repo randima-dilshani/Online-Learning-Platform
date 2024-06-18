a9395a2c72e5cc6627099b6cff5b58b45109ed54
Randima Dilshani <randimadilshani98@gmail.com>
2024-06-18 21:53:08 +0530

    Configure frontend and implement the landing page

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./componenets/User/Login";
+import LandingPage from "./pages/LandingPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
+
+export default App;
